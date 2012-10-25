20baebd2bcad2e4ca6c3b26de5d9c98b8aafb9a5
Mitchell Ludwig <mitch@edgemontgeek.com>
2012-10-25 11:32:31 -0600

    Decided to keep *error* and instead call it *intended behavior*

--- 501-A2-Reflection/src/InspectorTest.java
+++ 501-A2-Reflection/src/InspectorTest.java
@@ -40,14 +40,27 @@ public class InspectorTest {
 		Primes[][] paa = new Primes[10][5];
 		String sExpect = "asg2.Primes";
 		assertEquals(sExpect, i.getNiceName(p.getClass()));
-		for (Primes pInner : pa) {
-			pInner = new Primes(1000);
-		}
 		assertEquals(sExpect + "[]", i.getNiceName(pa.getClass()));
 		assertEquals(sExpect + "[][]", i.getNiceName(paa.getClass()));
 		sExpect = "Interface java.util.Set";
 		assertEquals(sExpect, i.getNiceName(java.util.Set.class));
 		sExpect = "";
 		assertEquals(sExpect, i.getNiceName(null));
+	}
+	
+	@Test
+	public void testConstructorParameters() throws NoSuchMethodException, SecurityException {
+		Inspector i = new Inspector();
+		Primes p = new Primes(1000);
+		assertEquals("", i.constructorParameters(p.getClass().getConstructor(null)));
+		assertEquals("int", i.constructorParameters(p.getClass().getConstructor(int.class)));
+	}
+	
+	@Test
+	public void testMethodExceptions() throws NoSuchMethodException, SecurityException {
+		Inspector i = new Inspector();
+		Primes p = new Primes(1000);
+		assertEquals("throws java.lang.Exception", i.methodExceptions(p.getClass().getMethod("areCoprime", int.class, int.class)));
+		assertEquals("", i.methodExceptions(p.getClass().getMethod("isPrime", int.class)));
 	}
 }
