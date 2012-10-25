e3964a108d8540aa2edee0a6998b392f216ca514
Mitchell Ludwig <mitch@edgemontgeek.com>
2012-10-25 10:42:41 -0600

    Added tabs test

--- 501-A2-Reflection/src/InspectorTest.java
+++ 501-A2-Reflection/src/InspectorTest.java
@@ -1,13 +1,33 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-
 
 public class InspectorTest {
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testTabs() {
+		Inspector i = new Inspector();
+		assertEquals(0, i._iTabLevel);
+		i.tabIn();
+		assertEquals(1, i._iTabLevel);
+		System.out.println("Verify that there is 1 tab here");
+		i.forTheRecordLn("Test");
+		i.tabIn();
+		assertEquals(2, i._iTabLevel);
+		System.out.println("Verify that there are 2 tabs here");
+		i.forTheRecordLn("Test for 2");
+		i.tabIn();
+		assertEquals(3, i._iTabLevel);
+		i.tabIn();
+		assertEquals(4, i._iTabLevel);
+		i.tabOut();
+		assertEquals(3, i._iTabLevel);
+		i.tabOut();
+		assertEquals(2, i._iTabLevel);
+		i.tabOut();
+		assertEquals(1, i._iTabLevel);
+		i.tabOut();
+		assertEquals(0, i._iTabLevel);
 	}
 	
 }
