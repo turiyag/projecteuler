0fcf33181096a452a1ca81fb1d9e726d5c7ddc93
Mitchell Ludwig <mitch@edgemontgeek.com>
2012-10-25 10:58:53 -0600

    Added test for getNiceName, found error

--- 501-A2-Reflection/src/InspectorTest.java
+++ 501-A2-Reflection/src/InspectorTest.java
@@ -2,6 +2,8 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import asg2.Primes;
+
 public class InspectorTest {
 	
 	@Test
@@ -30,4 +32,22 @@ public class InspectorTest {
 		assertEquals(0, i._iTabLevel);
 	}
 	
+	@Test
+	public void testGetNiceName() {
+		Inspector i = new Inspector();
+		Primes p = new Primes(1000);
+		Primes[] pa = new Primes[100];
+		Primes[][] paa = new Primes[10][5];
+		String sExpect = "asg2.Primes";
+		assertEquals(sExpect, i.getNiceName(p.getClass()));
+		for (Primes pInner : pa) {
+			pInner = new Primes(1000);
+		}
+		assertEquals(sExpect + "[]", i.getNiceName(pa.getClass()));
+		assertEquals(sExpect + "[][]", i.getNiceName(paa.getClass()));
+		sExpect = "Interface java.util.Set";
+		assertEquals(sExpect, i.getNiceName(java.util.Set.class));
+		sExpect = "";
+		assertEquals(sExpect, i.getNiceName(null));
+	}
 }
